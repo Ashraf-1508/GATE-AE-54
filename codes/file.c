645043c53b80f65352c18d4c18d1d60c695bf37e
Ashraf-1508 <ayaanashraf69@gmail.com>
2024-02-19 23:28:06 +0530

    Updated

--- codes/file.c
+++ codes/file.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define NUM_POINTS 100
+#define NUM_POINTS 10000
 
 int main() {
     FILE *file;
@@ -15,7 +15,7 @@ int main() {
     // Generate data points and write to file
     double x, result;
     double start = 1.0;
-    double end = 10.0;
+    double end = 5.0;
     double step = (end - start) / (NUM_POINTS - 1);
 
     for (int i = 0; i < NUM_POINTS; i++) {
